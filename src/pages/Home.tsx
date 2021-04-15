b424e4de343d90fd445694c5d4eb3250f3a8852d
Jeeva111 <jeeva.deee111@gmail.com>
2021-04-15 08:26:11 +0530

    Drawing added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,21 +5,18 @@ import './Home.css';
 
 const Home: React.FC = () => {
 
-    // reference canvas element (with id="c")
+    // reference canvas element
     var canvasRef = useRef<HTMLCanvasElement>(null);
-    var canvas = new fabric.Canvas(canvasRef.current!);
+    var canvas = null;
     useEffect(()=> {
         initDrawPad();
     },[])
 
     const initDrawPad = () => {
-        // get 2d context to draw on (the "bitmap" mentioned earlier)
-        var circle = new fabric.Circle({
-            radius: 20, fill: 'green', left: 100, top: 100
+
+        canvas = new fabric.Canvas(canvasRef.current!,{
+            isDrawingMode:true
         });
-        
-        // "add" rectangle onto canvas
-        canvas.add(circle);
     }   
 
     return (
@@ -30,7 +27,10 @@ const Home: React.FC = () => {
                 </IonToolbar>
             </IonHeader>
             <IonContent fullscreen>
-                <canvas ref={canvasRef}/>
+                <canvas 
+                    ref={canvasRef}
+                    style={{height:"100%", width:"100%"}}
+                />
             </IonContent>
         </IonPage>
     );
